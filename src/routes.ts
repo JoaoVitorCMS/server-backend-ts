2d3cb7387518227b19e7334cde78f408421ae7f2
João Vitor Costa de Moura Santos <joaovitorcms1@gmail.com>
2025-05-27 16:44:48 -0300

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -2,71 +2,79 @@ import { Router, Request, Response } from "express";
 import userControllers from "./controllers/user";
 import user from "./controllers/user";
 const routes = Router();
-
+import listControllers from "./controllers/list";
 //rota de teste
 
-routes.get("/",(req: Request, res: Response): any => {
-    return res.send('rota de teste')
-})
+// routes.get("/",(req: Request, res: Response): any => {
+//     return res.send('rota de teste')
+// })
 
-//rota que retorna um json
-routes.get("/funcionarios", (req: Request, res: Response): any =>{
-    return res.json({
-        item: [
-            {
-             id: "1",
-             nome: "Joao",
-             cargo: "aluno",
-             idade: 20,
-             cursoPorHora: 120.00,
-             temLicenca: false
-            },
-            {
-                id: "2",
-                nome: "Daniel",
-                cargo: "aluno",
-                idade: 17,
-                cursoPorHora: 200.00,
-                temLicenca: true
-               },
-               {
-                id: "3",
-                nome: "Arthur",
-                cargo: "aluno",
-                idade: 17,
-                cursoPorHora: 80.00,
-                temLicenca: false
-               },
-        ]
-    })
-})
+// //rota que retorna um json
+// routes.get("/funcionarios", (req: Request, res: Response): any =>{
+//     return res.json({
+//         item: [
+//             {
+//              id: "1",
+//              nome: "Joao",
+//              cargo: "aluno",
+//              idade: 20,
+//              cursoPorHora: 120.00,
+//              temLicenca: false
+//             },
+//             {
+//                 id: "2",
+//                 nome: "Daniel",
+//                 cargo: "aluno",
+//                 idade: 17,
+//                 cursoPorHora: 200.00,
+//                 temLicenca: true
+//                },
+//                {
+//                 id: "3",
+//                 nome: "Arthur",
+//                 cargo: "aluno",
+//                 idade: 17,
+//                 cursoPorHora: 80.00,
+//                 temLicenca: false
+//                },
+//         ]
+//     })
+// })
 
-//rota que retorna um json com imagem
-routes.get("/imagem", (req: Request, res: Response): any =>{
-    return res.json({
-        item: [
-            {
-             id: "1",
-             nome: "Joao",
-            imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
-            },
-            {
-            id: "2",
-            nome: "Daniel",
-            imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
-               },
-               {
-                id: "3",
-                nome: "Arthur",
-                imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
-               },
-        ]
-    })
-})
+// //rota que retorna um json com imagem
+// routes.get("/imagem", (req: Request, res: Response): any =>{
+//     return res.json({
+//         item: [
+//             {
+//              id: "1",
+//              nome: "Joao",
+//             imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
+//             },
+//             {
+//             id: "2",
+//             nome: "Daniel",
+//             imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
+//                },
+//                {
+//                 id: "3",
+//                 nome: "Arthur",
+//                 imagem: "https://ichef.bbci.co.uk/ace/ws/640/amz/worldservice/live/assets/images/2015/09/26/150926165742__85730600_monkey2.jpg.webp",
+//                },
+//         ]
+//     })
+// })
+
 
 routes.post("/users", (req: Request, res: Response): any => userControllers.create(req, res))
 routes.get("/users", (req: Request, res: Response): any => userControllers.read(req, res))
 routes.put("/users/:id", (req: Request, res: Response): any => userControllers.updade(req, res))
 routes.delete("/users/:id", (req: Request, res: Response): any => userControllers.delete(req, res))
 routes.post("/login", (req: Request, res: Response): any => userControllers.login(req, res))
+
+//rotas listas
+
+routes.post("/list", (req: Request, res: Response) : any => listControllers.create(req, res))
+routes.get("/list", (req: Request, res: Response) : any => listControllers.read(req, res))
+routes.put("/list/:id", (req: Request, res: Response) : any => listControllers.update(req, res))
+routes.delete("/list/:id", (req: Request, res: Response) : any => listControllers.delete(req,res))
 export default routes;
